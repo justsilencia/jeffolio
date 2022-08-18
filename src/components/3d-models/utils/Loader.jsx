8f524e91aea572086a1347d251372ac9f97df0bc
justsilencia <silencedogood848@gmail.com>
2022-08-18 09:50:30 -0500

    Troubleshooting the suspense fallback for 3d models.

--- src/components/3d-models/utils/Loader.jsx
+++ src/components/3d-models/utils/Loader.jsx
@@ -4,12 +4,7 @@ import { Html } from '@react-three/drei';
 export default function Loader() {
     return (
         <>
-        <Html
-            prepend
-            center>
-            <div className="loader">
-                Loader...
-            </div>
+        <Html wrapperClass='loader'>
         </Html>
         </>
     )
