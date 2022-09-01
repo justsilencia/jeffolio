0771e999699e1572e2def7194a674cd1665f8be3
justsilencia <silencedogood848@gmail.com>
2022-09-01 13:27:49 -0500

    Fixed path in gatsby-3d model component. Deleted rogue file in the static/models directory.

--- src/components/3d-models/Gatsby-3d.jsx
+++ src/components/3d-models/Gatsby-3d.jsx
@@ -1,32 +1,33 @@
-
-import React, { useRef } from 'react';
-import { useGLTF } from '@react-three/drei';
-import { useThree, useFrame } from '@react-three/fiber';
-import AnimateHover from './utils/AnimateHover';
-import modelMods from './utils/modelMods';
+import React, { useRef } from "react"
+import { useGLTF } from "@react-three/drei"
+import { useThree, useFrame } from "@react-three/fiber"
+import AnimateHover from "./utils/AnimateHover"
+import modelMods from "./utils/modelMods"
 
 export default function Gatsby3d(props) {
+  const modelRef = useRef()
+  const { nodes, materials } = useGLTF("/models/gatsby-3d.glb")
 
-  const modelRef = useRef();
-  const { nodes, materials } = useGLTF('/models/gatsby-3d.glb');
-
-  useThree(({camera}) => {
-    camera.position.y = modelMods.cameraY;
-    camera.lookAt(...modelMods.cameraLookat);
-  });
+  useThree(({ camera }) => {
+    camera.position.y = modelMods.cameraY
+    camera.lookAt(...modelMods.cameraLookat)
+  })
 
   useFrame(() => {
-    (props.animateProps.animateMode === 1) ? 
-      AnimateHover(modelRef, props.animateProps) 
-      : 
-      modelRef.current.rotation.z += modelMods.zRotate;
-  });
+    props.animateProps.animateMode === 1
+      ? AnimateHover(modelRef, props.animateProps)
+      : (modelRef.current.rotation.z += modelMods.zRotate)
+  })
 
   return (
-      <group ref={modelRef} {...props} dispose={null}>
-        <mesh geometry={nodes.Circle.geometry} material={materials['Material.001']} scale={5.29} />
-      </group>
+    <group ref={modelRef} {...props} dispose={null}>
+      <mesh
+        geometry={nodes.Circle.geometry}
+        material={materials["Material.001"]}
+        scale={5.29}
+      />
+    </group>
   )
 }
 
-useGLTF.preload('/models/gatsby-3d-chosen.glb');
+useGLTF.preload("/models/gatsby-3d.glb")
