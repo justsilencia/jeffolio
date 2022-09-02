2ecdb2380d83aaf63ec8ddcfb2875cf3cb1cbf28
justsilencia <silencedogood848@gmail.com>
2022-09-02 14:55:42 -0500

    Added Upwork reviews to the navigation pane.

--- src/components/nav/CustNavbar.jsx
+++ src/components/nav/CustNavbar.jsx
@@ -4,9 +4,9 @@ import {
   faHome,
   faFolderOpen,
   faCog,
-  faCommentDots,
   faQuestion,
   faBook,
+  faStar,
 } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
@@ -34,12 +34,45 @@ class CustNavbar extends Component {
           <FontAwesomeIcon icon={faQuestion} />
           <span>WHY ME</span>
         </a>
-        {/* <a id='stats' href='/Feedback'>
-                    <FontAwesomeIcon icon={faCommentDots} />
-                    <span>
-                        FEEDBACK
-                    </span>
-                </a> */}
+        <a
+          style={{ textDecoration: "none" }}
+          target="_child"
+          href="https://www.upwork.com/freelancers/~015e4b636a42e26d33?viewMode=1"
+        >
+          <div className="nav-reviews">
+            <div className="review">
+              <FontAwesomeIcon style={{ color: "green" }} icon={faStar} />
+              <FontAwesomeIcon style={{ color: "green" }} icon={faStar} />
+              <FontAwesomeIcon style={{ color: "green" }} icon={faStar} />
+              <FontAwesomeIcon style={{ color: "green" }} icon={faStar} />
+              <FontAwesomeIcon style={{ color: "green" }} icon={faStar} />{" "}
+              <b>5.00</b>
+              <span className="review-date">May 18, 2022 - Present</span>
+              <p>
+                "Jeff is awesome! We work with him for tech writing, mainly
+                about Javascript. All his work is amazing and on point. He is
+                the driving force behind our company blog. He includes code
+                examples, amazing writing, what else can you ask for?"
+              </p>
+            </div>
+            <div className="review">
+              <FontAwesomeIcon style={{ color: "green" }} icon={faStar} />
+              <FontAwesomeIcon style={{ color: "green" }} icon={faStar} />
+              <FontAwesomeIcon style={{ color: "green" }} icon={faStar} />
+              <FontAwesomeIcon style={{ color: "green" }} icon={faStar} />
+              <FontAwesomeIcon style={{ color: "green" }} icon={faStar} />{" "}
+              <b>5.00</b>
+              <span className="review-date">Apr 1, 2022 - Apr 21, 2022</span>
+              <p>
+                "I hired Jeff to write several technical programming articles
+                for my blog, and he knocked it out of the park! He has a
+                technical background with knowledge of Git and programming
+                languages and has great communication skills.... Highly
+                recommended!"
+              </p>
+            </div>
+          </div>
+        </a>
       </Menu>
     )
   }
